2c49a2909d828335213bc10cceeb3cac13ca9c2d
Maris <guillermo.mariscal.zaballa@gmail.com>
2015-01-11 15:52:14 +0100

    Full project

---
+++ RecordReader/lib/Algorithm.java
@@ -0,0 +1,38 @@
+/*package mapReduce;
+
+import java.io.File;
+
+public class Algorithm
+{
+	private static File f;
+	private static Object[] jobs;
+	private static Object hypergraphV = 0;
+	private static Object hypergraphE = 0;
+	private static Object intermeadiateKey;
+	private static long T;
+	private static long K;
+
+	public static void main(String[]args)
+	{
+		for(Object tmpJob : jobs)
+		{
+			T = 0;
+			K = 0;
+			for()
+			{
+				if(file)
+				{
+					for()
+					{
+						
+					}
+				}
+			}
+			for(Object intermediateKey : K)
+			{
+				
+			}
+		}
+	}
+}
+*/
